abe7a04cf167667c4235b4e2f7ba5ea3b9ff97b7
IstiakIqbal <istiakiqbal@gmail.com>
2023-08-07 18:37:47 +0600

    Loader added

--- src/App.js
+++ src/App.js
@@ -8,22 +8,33 @@ import About from './Components/Pages/PublicPages/About/About';
 import Hotels from './Components/Pages/PublicPages/Hotels/Hotels';
 import Tours from './Components/Pages/PublicPages/Tours/Tours';
 import data from './data/destinationsData.json';
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import Loading from './Components/Pages/Loading/Loading';
 
 export const destinationContext = createContext([]);
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  })
   return (
     <>
-      <Routes>
-        <Route path='/' element={<destinationContext.Provider value={data.data}><Home></Home></destinationContext.Provider>}></Route>
-        <Route path='/destinations' element={<destinationContext.Provider value={data}><Destinations></Destinations></destinationContext.Provider>}></Route>
-        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='/hotels' element={<Hotels></Hotels>}></Route>
-        <Route path='/tours' element={<Tours></Tours>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
-      </Routes>
+      {
+        isLoading ? (<Loading />) : (
+          <Routes>
+            <Route path='/' element={<destinationContext.Provider value={data.data}><Home></Home></destinationContext.Provider>}></Route>
+            <Route path='/destinations' element={<destinationContext.Provider value={data}><Destinations></Destinations></destinationContext.Provider>}></Route>
+            <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+            <Route path='/about' element={<About></About>}></Route>
+            <Route path='/hotels' element={<Hotels></Hotels>}></Route>
+            <Route path='/tours' element={<Tours></Tours>}></Route>
+            <Route path='*' element={<NotFound></NotFound>}></Route>
+          </Routes>
+        )
+      }
     </>
   );
 }
--- src/Components/Pages/PublicPages/About/About.js
+++ src/Components/Pages/PublicPages/About/About.js
@@ -10,11 +10,11 @@ const About = () => {
         <div className='bg-black text-white'>
             <Navbar></Navbar>
             <Banner url={imgUrl} alt={'Banner of about routes'}></Banner>
-            <div className='absolute top-56 w-full'><Search></Search></div>
+            {/* <div className='absolute top-56 w-full'><Search></Search></div> */}
             <div className='absolute top-[30rem] w-full mt-24 md:mt-0 px-2 md:px-0'>
                 <div className='max-w-6xl mx-auto'>
-                    <h1 className='text-2xl md:text-3xl font-medium text-[#ff7f00]'>Most Popular</h1>
-                    <h1 className='text-3xl md:text-5xl font-semibold mt-3'>Holiday Destinations</h1>
+                    {/* <h1 className='text-2xl md:text-3xl font-medium text-[#ff7f00]'>Most Popular</h1>
+                    <h1 className='text-3xl md:text-5xl font-semibold mt-3'>Holiday Destinations</h1> */}
                     {/* <div className="card-container mt-10 md:grid grid-cols-2 gap-x-7 gap-y-10">
                         {
                             value.data.map((data, idx) => <Destination key={idx} data={data}></Destination>)
